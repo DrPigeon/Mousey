44b45b47edf85205bb66a28d7536d872ab3e3bc1
FrostLuma <FrostLuma@gmail.com>
2017-09-24 06:54:37 +0200

    utils/web: fix typo, add missing param to docstring

--- mousey/utils/web.py
+++ mousey/utils/web.py
@@ -12,6 +12,8 @@ async def get_json(url, *, session=None, headers=None, **kwargs):
     ----------
     url : str
         The url to download the json from
+    headers : dict
+        The request headers to use. Note that the User-Agent is pre set and will be overwritten.
     session : Optional[aiohttp.ClientSession]
         The session to use for the request
 
@@ -25,8 +27,8 @@ async def get_json(url, *, session=None, headers=None, **kwargs):
     session = session or aiohttp.ClientSession()
     headers = headers or {}
 
-    headers.update({'User-Agent':USER_AGENT})
+    headers.update({'User-Agent': USER_AGENT})
 
     async with session.get(url, headers=headers, **kwargs) as resp:
-        # ignoring content type as Mousey does a request which returns the from content type
+        # ignoring content type as Mousey does a request which returns the wrong content type
         return await resp.json(content_type=None)
