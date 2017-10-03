a3ce200ce3e2f84575fe8bd7964a671c3b367fb6
FrostLuma <FrostLuma@gmail.com>
2017-10-03 18:17:38 +0200

    utils: add haste function

--- mousey/utils/__init__.py
+++ mousey/utils/__init__.py
@@ -4,4 +4,4 @@ from .converters import *
 from .formatting import clean_formatting, clean_mentions, clean_text, name_id, Table
 from .misc import shell
 from .time import human_delta, OptionalTime, Time, Timer
-from .web import get_json
+from .web import get_json, haste
--- mousey/utils/web.py
+++ mousey/utils/web.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 import aiohttp
 
+from config import PASTEE_TOKEN
 from mousey.const import USER_AGENT
+
+
+PASTE_ENDPOINT = 'https://api.paste.ee/v1/pastes'
 
 
 async def get_json(url, *, session=None, headers=None, **kwargs):
@@ -32,3 +36,30 @@ async def get_json(url, *, session=None, headers=None, **kwargs):
     async with session.get(url, headers=headers, **kwargs) as resp:
         # ignoring content type as Mousey does a request which returns the wrong content type
         return await resp.json(content_type=None)
+
+
+async def haste(content: str, *, session=None) -> str:
+    """
+    Creates a haste and returns the link to it.
+
+    Parameters
+    ----------
+    content : str
+        The content which gets put into the haste
+    session : Optional[aiohttp.ClientSession]
+        The session to use for the request
+
+    Returns
+    -------
+    str
+        The url to the haste
+    """
+    session = session or aiohttp.ClientSession()
+    headers = {'User-Agent': USER_AGENT, 'X-Auth-Token': PASTEE_TOKEN}
+
+    payload = {"sections": [{"name": "Mousey Haste", "contents": content}]}
+
+    async with session.post(PASTE_ENDPOINT, headers=headers, json=payload) as resp:
+        res = await resp.json()
+
+    return res.get('link')
