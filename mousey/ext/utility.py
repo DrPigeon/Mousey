e3439b9441fd3d97f55d580ba1233c752b846007
FrostLuma <FrostLuma@gmail.com>
2017-10-04 17:09:49 +0200

    ext/utility: add prefix management

--- mousey/ext/utility.py
+++ mousey/ext/utility.py
@@ -2,7 +2,7 @@
 import discord
 
 from mousey import Cog, commands, Context, Mousey
-from mousey.const import FEEDBACK_CHANNEL
+from mousey.const import DENY_EMOJI, FEEDBACK_CHANNEL
 
 
 class Utility(Cog):
@@ -25,6 +25,65 @@ class Utility(Cog):
 
         channel = self.mousey.get_channel(FEEDBACK_CHANNEL)
         await channel.send(embed=embed)
+        await ctx.ok()
+
+    @commands.group(aliases=['prefixes'])
+    @commands.guild_only()
+    async def prefix(self, ctx: Context):
+        """Manage and view prefixes."""
+        config = await self.mousey.config.get(ctx.guild.id)
+
+        if 'prefixes' not in config:
+            config['prefixes'] = []
+
+        prefixes = config['prefixes'] + [f'<@{self.mousey.user.id}> ']
+
+        # put prefixes in quotes as they may have trailing spaces
+        pretty_prefixes = '\n'.join(f'"{x}"' for x in prefixes)
+
+        await ctx.send(f'Current usable prefixes:\n{pretty_prefixes}')
+
+    @prefix.command(name='add')
+    @commands.guild_only()
+    @commands.has_permissions(check_bot=False, manage_guild=True)
+    async def prefix_add(self, ctx: Context, prefix: str):
+        """Add a new prefix. To have trailing spaces put it in quotes."""
+        config = await self.mousey.config.get(ctx.guild.id)
+
+        if 'prefixes' not in config:
+            config['prefixes'] = []
+
+        if len(prefix) > 20:
+            return await ctx.send(f'{DENY_EMOJI} prefixes may only be 20 characters long!')
+
+        if len(config['prefixes']) > 10:
+            return await ctx.send(f'{DENY_EMOJI} you can only add 10 custom prefixes!')
+
+        # just give the illusion we added it again, even if it exists
+        if prefix not in config['prefixes']:
+            config['prefixes'].append(prefix)
+            await self.mousey.config.save(ctx.guild.id)
+
+        await ctx.send(f'Added "{prefix}" as a prefix', delete_after=10)
+        await ctx.ok()
+
+    @prefix.command(name='remove')
+    @commands.guild_only()
+    @commands.has_permissions(check_bot=False, manage_guild=True)
+    async def prefix_remove(self, ctx: Context, prefix: str):
+        """Remove a prefix. To remove a prefix with trailing spaces put it in quotes."""
+        config = await self.mousey.config.get(ctx.guild.id)
+
+        if 'prefixes' not in config:
+            config['prefixes'] = []
+
+        if prefix not in config['prefixes']:
+            return await ctx.send(f'{DENY_EMOJI} "{prefix}" is not a valid prefix!')
+
+        config['prefixes'].remove(prefix)
+        await self.mousey.config.save(ctx.guild.id)
+
+        await ctx.send(f'Removed "{prefix}" from prefixes.', delete_after=10)
         await ctx.ok()
 
     @commands.command(aliases=['oauth'])
