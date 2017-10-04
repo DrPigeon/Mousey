c9f1a60a77903aaf309a37c6d64cb0da090ab765
FrostLuma <FrostLuma@gmail.com>
2017-10-04 17:45:05 +0200

    utils: fix ImportError

--- mousey/utils/__init__.py
+++ mousey/utils/__init__.py
@@ -3,5 +3,5 @@ from .checks import is_admin
 from .converters import *
 from .formatting import clean_formatting, clean_mentions, clean_text, name_id, Table
 from .misc import shell
-from .time import human_delta, OptionalTime, Time, Timer
+from .time import human_delta, Time, Timer
 from .web import get_json, haste
