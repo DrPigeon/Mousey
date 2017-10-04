7f9a40168cab7a75614d8c8911aea478eb7f3f18
FrostLuma <FrostLuma@gmail.com>
2017-10-04 16:45:47 +0200

    ext/core: add name, icon to guilds table, don't set an empty config

--- mousey/ext/core/state.py
+++ mousey/ext/core/state.py
@@ -8,60 +8,65 @@ class State(Cog):
     """Cog to keep the database updated."""
 
     async def on_ready(self):
-        pass  # todo: resync db
+        await self.sync()
 
     async def on_guild_join(self, guild: discord.Guild):
         async with self.db.acquire() as conn:
             query = """
                 INSERT INTO guilds (
-                    guild_id, created_at, joined_at, owner_id, config
+                    guild_id, created_at, name, icon, joined_at, owner_id
                 )
                 VALUES (
-                    $1, $2, $3, $4, $5
+                    $1, $2, $3, $4, $5, $6
                 )
                 ON CONFLICT (
                     guild_id
                 )
-                DO UPDATE SET joined_at = $3, owner_id = $4
+                DO UPDATE SET name = $3, icon = $4, joined_at = $5, owner_id = $6
             """
-            await conn.execute(query, guild.id, guild.created_at, guild.me.joined_at, guild.owner_id, '{}')
+            await conn.execute(
+                query, guild.id, guild.created_at, guild.name, guild.icon, guild.me.joined_at, guild.owner_id,
+            )
 
     async def on_guild_update(self, before: discord.Guild, after: discord.Guild):
-        if before.owner == after.owner:
+        if before.name == after.name and before.icon == after.icon and before.owner == after.owner:
             return
 
         async with self.db.acquire() as conn:
-            query = 'UPDATE guilds SET owner_id = $2 WHERE guild_id = $1'
-            await conn.execute(query, before.id, after.owner_id)
+            query = 'UPDATE guilds SET name = $1, icon = $2, owner_id = $3 WHERE guild_id = $4'
+            await conn.execute(query, after.name, after.icon, after.owner_id, after.id)
 
     async def on_guild_remove(self, guild: discord.Guild):
-        # guilds get kept because the config should be preserved
+        # guilds get kept to preserve the config
         async with self.db.acquire() as conn:
             query = 'UPDATE guilds SET joined_at = NULL WHERE guild_id = $1'
             await conn.execute(query, guild.id)
 
-    @commands.command(typing=True)
+    @commands.command(name='sync', typing=True)
     @commands.is_owner()
-    async def sync(self, ctx: Context):
+    async def sync_(self, ctx: Context):
         """Syncs Mouseys state to the database."""
+        await self.sync()
+        await ctx.ok()
+
+    async def sync(self):
         async with self.db.acquire() as conn:
             query = """
                 INSERT INTO guilds (
-                    guild_id, created_at, joined_at, owner_id, config
+                    guild_id, created_at, name, icon, joined_at, owner_id
                 )
                 VALUES (
-                    $1, $2, $3, $4, $5
+                    $1, $2, $3, $4, $5, $6
                 )
                 ON CONFLICT (
                     guild_id
                 )
-                DO UPDATE SET joined_at = $3, owner_id = $4
+                DO UPDATE SET name = $3, icon = $4, joined_at = $5, owner_id = $6
             """
-
-            guilds = ((x.id, x.created_at, x.me.joined_at, x.owner_id, '{}') for x in self.mousey.guilds)
+            guilds = (
+                (x.id, x.created_at, x.name, x.icon, x.me.joined_at, x.owner_id) for x in self.mousey.guilds
+            )
             await conn.executemany(query, guilds)
-
-        await ctx.ok()
 
 
 def setup(mousey: Mousey):
